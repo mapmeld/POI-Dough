33a28bd52861225cc074eb166359df2ac5c39042
Nick Doiron <ndoiron@mapmeld.com>
2012-05-27 11:05:02 -0700

    bootswatch tabs interface for procedure

--- public/scripts/kansas.js
+++ public/scripts/kansas.js
@@ -1,4 +1,20 @@
 function syntaxCheck(){
+  switchToCode();
   document.getElementById("codestore").innerHTML = "<pre class='brush:jscript'>" + document.getElementById("codedraft").value + "</pre>";
   SyntaxHighlighter.highlight();
+}
+function storeProcedure(){
+  document.getElementById("codeform").submit();
+}
+function switchToCode(){
+  document.getElementById("codeTab").className = "active";
+  document.getElementById("editTab").className = "";
+  document.getElementById("currentcode").style.display = "block";
+  document.getElementById("editcode").style.display = "none";
+}
+function switchToEdit(){
+  document.getElementById("codeTab").className = "";
+  document.getElementById("editTab").className = "active";
+  document.getElementById("currentcode").style.display = "none";
+  document.getElementById("editcode").style.display = "block";
 }
