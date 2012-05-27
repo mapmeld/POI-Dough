2c9cd87d30a30ead58d58ef36551d0ce3d9bee5a
Nick Doiron <ndoiron@mapmeld.com>
2012-05-26 23:02:00 -0700

    viewing and saving procedures

--- public/scripts/shCore.js
+++ public/scripts/shCore.js
@@ -756,7 +756,7 @@ function parseParams(str)
 
 	while ((match = regex.exec(str)) != null) 
 	{
-		var value = match
+		var value = match.input
 			.replace(/^['"]|['"]$/g, '') // strip quotes from end of strings
 			;
 		
