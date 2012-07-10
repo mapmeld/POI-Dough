5458a114d811208eb659b2034f5944bbf7a5e118
Nick Doiron <ndoiron@mapmeld.com>
2012-07-09 20:02:31 -0700

    prevent doubling parks when changing texture

--- public/scripts/myeditor.js
+++ public/scripts/myeditor.js
@@ -906,7 +906,7 @@ function setEffect(wayid, settype){
   }
   if(settype.indexOf("2D") > -1){
     var p_index = parks.length;
-    if(!promoted[wayid].effect || promoted[wayid].effect != settype.replace("2D","")){
+    if(promoted[wayid].effect.indexOf("2D") == -1 || promoted[wayid].effect != settype){
       parks.push({
         wayid: wayid,
 	    /* vertices: promoted[wayid].osmdata.line.splice(0), */
